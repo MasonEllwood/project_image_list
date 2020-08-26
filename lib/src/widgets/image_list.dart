10e32c8a980d5f77eae1c724ad27156994eab644
Mason Ellwood <masonellwood@gmail.com>
2020-08-26 10:11:35 -0700

    added border title and padding

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -10,11 +10,30 @@ class ImageList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: ListView.builder(
-        itemCount: this.images.length,
+        itemCount: images.length,
         itemBuilder: (BuildContext context, int index) {
-          return Image.network(this.images[index].url);
+          return buildImage(images[index]);
        },
       ),
+    );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.cyan),
+      ),
+      padding: EdgeInsets.all(20.0),
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          Image.network(image.url),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Text(image.title),
+          ),
+        ],
+      )
     );
   }
 }
